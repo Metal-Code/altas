52586dae052604a368d5497872cad824b2f289c6
Ayush Kumar <metalbond69@gmail.com>
2026-06-23 23:29:39 +0530

    config and security done

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
-    secret_key: str
-    algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
+    TOKEN_SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS : int 
 
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
+
